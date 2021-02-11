4a002a6ff8eb651b7f42b816ffacfb1180e877f3
liyaSileshi <liyasileshi@gmail.com>
2021-02-11 15:07:07 -0800

    deploy trial 3

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from flask import request, json
 import requests
 # local url
-url = 'http://127.0.0.1:5000' # change to your url
+# url = 'http://127.0.0.1:5000' # change to your url
+# heroku_url = 'https://titanic-model-flask.herokuapp.com'
 
+heroku_url = "https://git.heroku.com/titanic-model-flask.git"
 # sample data
 data = {'Pclass': 3
       , 'Age': 2
@@ -10,7 +12,8 @@ data = {'Pclass': 3
       , 'Fare': 50}
 data = json.dumps(data)
 
-send_request = requests.post(url, data)
+send_request = requests.post(heroku_url, data)
 
 print(send_request)
 print(send_request.json())
+requests.get(heroku_url).json()
